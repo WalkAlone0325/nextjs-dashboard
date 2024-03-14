9c0fdd2561468a9325e854d4484a0db9349cd207
Nebula <jing.me@icloud.com>
2024-03-14 17:37:39 +0800

    feat: add pages

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inder, Lusitana } from 'next/font/google';
+
+export const inter = Inder({
+  subsets: ['latin'],
+  weight: '400',
+});
+
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: '400',
+});
